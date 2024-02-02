e07b415d7a1dfdc5a6fc438421f154340fab8f9c
Emil Dziadczyk <emil.dziadczyk@tomtom.com>
2024-02-02 12:46:28 +0100

    new version

--- showLayerContact.py
+++ showLayerContact.py
@@ -23,7 +23,7 @@ filtered_layers = [layer for layer in data['layers'] if layer['layer_id'] in arg
 print(f"===================================================")
 
 for layer in filtered_layers:
-     print(f"Layer ID: {layer['layer_id']}, Description: {layer['description']}")
-#      print(f"Layer ID: {layer['layer_id']}, Contact: {layer['contact']}")
-
+#     print(f"Layer ID: {layer['layer_id']}, Description: {layer['description']}")
+     print(f"Layer ID: {layer['layer_id']}, Contact: {layer['contact']}, Description: {layer['description']}")
+#      print(f"Contact: {layer['contact']}")
 print(f"===================================================")
